5e793f0160470dea0ae6b6506636d87f4ccf7d7b
Cameron Cosby <82902082+camcosby08@users.noreply.github.com>
2022-01-15 23:15:39 -0500

    Menu Completed
    Menu Completed

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Hit the quit button");
+        Application.Quit();
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OptionsScene()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+}
